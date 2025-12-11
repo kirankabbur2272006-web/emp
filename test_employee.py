583b816ddd78afd8cfcd3e6b55ec480394ed7bb7
kirankabbur2272006-web <kirankabbur2272006@gmail.com>
2025-12-11 15:15:45 +0530

    Refactor employee details test for consistency

--- test_employee.py
+++ test_employee.py
@@ -1,13 +1,9 @@
 from employee import employee_details
 def test_employee_details():
-    name = "Alice"
-    emp_id = "E1001"
-    department = "IT"
-    salary = 50000
-    expected_output = (
-        "Employee Name: Alice\n"
-        "Employee ID: E1001\n"
-        "Department: IT\n"
-        "Salary: 55000"
+    output=(
+        "Employee_Name:Alice\n"
+        "Employee_ID:E1001\n"
+        "Department:IT\n"
+        "Salary:55000"
     )
-    assert employee_details("alice","E1001","IT",50000) == expected_output
+    assert employee_details("Alice","E1001","IT",55000) == output
