5952999bf0b008395271b569afef88fd773fff5c
kirankabbur2272006-web <kirankabbur2272006@gmail.com>
2025-12-11 15:15:08 +0530

    Update employee.py

--- employee.py
+++ employee.py
@@ -1,14 +1,15 @@
-def employee_details(name,emp_id,departement,salary):
-   result=(
-        f"Employee Name: {name}\n"
-        f"Employee ID: {emp_id}\n"
-        f"Department: {departement}\n"
-        f"Salary: {salary}\n"
-   )
-   return result
-if__name= "__main__"
-name="allice"
-emp_id="E1001"
-departement="IT"     
-salary=50000
-print(employee_details(name, emp_id, departement, salary))
+def employee_details(name,emp_id,department,salary):
+    result = (
+        f"Employee_Name:{name}\n"
+        f"Employee_ID:{emp_id}\n"
+        f"Department:{department}\n"
+        f"Salary:{salary}"
+    )
+    return result
+
+if _name_ == "_main_":
+    name="Alice"
+    emp_id="E1001"
+    department="IT"
+    salary=55000
+    print(employee_details(name,emp_id,department,salary))
